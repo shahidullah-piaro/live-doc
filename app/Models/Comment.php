0c83de1de374f1a4b0ad7f5d791cd6dd355523ef
shahidullah Piaro <shahidullah.piyaro@gmail.com>
2024-03-10 10:55:15 +0600

    defined model relations, accessors and mutators

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,8 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'body',
+        'user_id',
+        'post_id',
+    ];
 
     protected $casts = [
         'body' => 'array'
     ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class, 'post_user', 'user_id', 'post_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'user_id');
+    }
 }
