f9d6bf896e8ff5912479c7912b0e0b4de24830b6
shahidullah Piaro <shahidullah.piyaro@gmail.com>
2024-03-13 15:47:26 +0600

    user repository-unit test

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -3,6 +3,9 @@
 
 namespace App\Repositories;
 
+use App\Events\Models\Post\PostCreated;
+use App\Events\Models\Post\PostDeleted;
+use App\Events\Models\Post\PostUpdated;
 use App\Exceptions\GeneralJsonException;
 use App\Models\Post;
 use Illuminate\Support\Facades\DB;
@@ -19,6 +22,7 @@ class PostRepository extends BaseRepository
                 'body' => data_get($attributes, 'body'),
             ]);
             throw_if(!$created, GeneralJsonException::class, 'Failed to create. ');
+            event(new PostCreated($created));
             if($userIds = data_get($attributes, 'user_ids')){
                 $created->users()->sync($userIds);
             }
@@ -40,6 +44,7 @@ class PostRepository extends BaseRepository
             ]);
 
             throw_if(!$updated, GeneralJsonException::class, 'Failed to update post');
+            //event(new PostUpdated($post));
 
             if($userIds = data_get($attributes, 'user_ids')){
                 $post->users()->sync($userIds);
@@ -60,7 +65,7 @@ class PostRepository extends BaseRepository
             $deleted = $post->forceDelete();
 
             throw_if(!$deleted, GeneralJsonException::class, "cannot delete post.");
-
+            event(new PostDeleted($post));
             return $deleted;
         });
 
